86d92be832286e337b353311485a3f8b51a97794
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-09-25 19:16:46 +0200

    first commit

---
+++ Onsdag/RestJAXRS/src/main/java/dto/PersonDTO.java
@@ -0,0 +1,98 @@
+package dto;
+
+import entities.Person;
+
+/**
+ *
+ * @author josef
+ */
+public class PersonDTO {
+
+    private int id;
+    private String fName;
+    private String lName;
+    private String phone;
+    private String street;
+    private String zip;
+    private String city;
+
+    public PersonDTO(Person p) {
+        this.fName = p.getFirstName();
+        this.lName = p.getLastName();
+        this.phone = p.getPhone();
+        this.id = p.getId();
+        this.street = p.getAddress().getStreet();
+        this.zip = p.getAddress().getZip();
+        this.city = p.getAddress().getCity();
+    }
+
+    public PersonDTO(String fn,String ln, String phone, String street, String zip, String city) {
+        this.fName = fn;
+        this.lName = ln;
+        this.phone = phone;        
+        this.street = street;
+        this.zip = zip;
+        this.city = city;
+    }
+
+    public PersonDTO() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ Onsdag/RestJAXRS/src/test/java/rest/PersonResourceTest.java
@@ -0,0 +1,144 @@
+package rest;
+
+import dto.PersonDTO;
+import entities.Address;
+import entities.Person;
+import utils.EMF_Creator;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import static org.glassfish.grizzly.http.util.Header.ContentType;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class PersonResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+    private static Person p1, p2, p3;
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        p1 = new Person("Josef", "Marc", "112");
+        p1.setAddress(new Address("vej", "2600", "Glostrup"));
+        p2 = new Person("Frederik", "Dahl", "30303030");
+        p2.setAddress(new Address("vej", "2200", "Nørrebro"));
+        p3 = new Person("Thor", "Christensen", "45454545");
+        p3.setAddress(new Address("vej", "4200", "Slagelse"));
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
+            em.createNamedQuery("Address.deleteAllRows").executeUpdate();
+            em.persist(p1);
+            em.persist(p2);
+            em.persist(p3);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/person").then().statusCode(200);
+    }
+
+    //This test assumes the database contains two rows
+    @Test
+    public void testDummyMsg() throws Exception {
+        given()
+                .contentType("application/json")
+                .get("/person/").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("msg", equalTo("Hello World"));
+    }
+
+    
+    @Test
+    public void addPerson(){
+        given()
+                .contentType("application/json")
+                .body(new PersonDTO("Jon", "Snow", "2112211","Bygaden 28","2100","Kbh Ø"))
+                .when()
+                .post("person")
+                .then()
+                .body("fName", equalTo("Jon"))
+                .body("lName", equalTo("Snow"))
+                .body("phone", equalTo("2112211"))
+                .body("id", notNullValue());
+    }
+    
+    @Test
+    public void getAllPersons(){
+            List<PersonDTO> personsDTOs;
+        
+            personsDTOs = given()
+                    .contentType("application/json")
+                    .when()
+                    .get("/person/all")
+                    .then()
+                    .extract().body().jsonPath().getList("all", PersonDTO.class);
+                    
+            PersonDTO p1DTO = new PersonDTO(p1);
+            PersonDTO p2DTO = new PersonDTO(p2);
+            PersonDTO p3DTO = new PersonDTO(p3);
+            
+            System.out.println(personsDTOs);
+            
+            assertEquals(personsDTOs.size(), 3);
+    }
+}
